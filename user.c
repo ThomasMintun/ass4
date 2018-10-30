786b2f0f5d5c740b51a65d262ea45a131880649d
ThomasMintun <tmm26b@mail.umsl.edu>
2018-10-30 15:07:52 -0500

    user.c file

---
+++ user.c
@@ -0,0 +1,14 @@
+#include "oss.h"
+
+int main(int argc, char* argv[]){
+
+    sharedMemoryConfig();
+
+    printf("Hello world\n");
+
+    fprintf(fp, "I am from user...")
+
+    shmdt(sysClockshmPtr);
+    shmdt(PCBshmPtr);
+    return 0;
+}
